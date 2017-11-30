ca5139da96a16d38784eeab57610cefda13fb6ca
Alexandr <a.horbach@godeltech.com>
2017-11-30 12:40:59 +0300

    Created table 'Wicked' with collum Id (Primary key, not null)

--- task01/file1.sql
+++ task01/file1.sql
@@ -12,3 +12,14 @@ CREATE DATABASE Something
 GO
 
 SELECT name, crdate FROM master.dbo.sysdatabases WHERE name LIKE '%Something%';
+
+
+IF OBJECT_ID('Wicked', 'U') IS NOT NULL
+DROP TABLE Wicked
+GO
+
+CREATE TABLE Wicked
+(
+    Id INT NOT NULL PRIMARY KEY,
+);
+GO
