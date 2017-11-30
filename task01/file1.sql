43f85c894ad8aa85b1bdbad914d50995934a3f2b
Alexandr <a.horbach@godeltech.com>
2017-11-30 12:21:09 +0300

    Created a new database called 'Something'. Selected it's name and creation date.

---
+++ task01/file1.sql
@@ -0,0 +1,14 @@
+-- Create a new database called 'Something'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT name
+        FROM sys.databases
+        WHERE name = N'Something'
+)
+CREATE DATABASE Something
+GO
+
+SELECT name, crdate FROM master.dbo.sysdatabases WHERE name LIKE '%Something%';
